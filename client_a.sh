8a04b4e5732f752d9977b1c03c365e76b7813882
Marijn Stollenga <m.stollenga@gmail.com>
2022-07-28 02:30:44 +0200

    server client scripts

---
+++ client_a.sh
@@ -0,0 +1,2 @@
+#!/bin/env sh
+./zig-out/bin/miner --ip 0.0.0.0 --port 8091 --remote_ip 0.0.0.0 --remote_port 8090 --plot_path merged_plot.db
---
+++ server.sh
@@ -0,0 +1,2 @@
+#!/bin/env sh
+./zig-out/bin/miner --ip 0.0.0.0 --port 8090 --plot_path merged_plot.db
