dfab61f0a367607a8954567953d926c3530dbde7
Marijn Stollenga <m.stollenga@gmail.com>
2022-08-09 20:45:01 +0200

    Fix log([]u8) logic

--- src/math.zig
+++ src/math.zig
@@ -20,26 +20,28 @@ pub fn log2(id: dht.ID) f64 {
         }
     }
 
-    if (zero_bits > id.len - 4) {
+    if (zero_bits > id.len * 8 - 4) {
         std.log.debug("Error, failure in log calculation, too many zeroes in distance: {}", .{hex(&id)});
         return 0;
     }
 
     var int_value: u32 = 0;
-    var int_representation = @ptrCast([*]u8, &int_value);
+    var int_representation = std.mem.asBytes(&int_value);
+
     var i: usize = 0;
     while (i < @sizeOf(u32)) : (i += 1) {
         const byte_index = zero_bits / 8 + i;
         const bit_index = @intCast(u3, zero_bits % 8);
-        int_representation[i] = (id[byte_index] << bit_index);
+        const repr_index = @sizeOf(u32) - 1 - i;
+        int_representation[repr_index] = (id[byte_index] << bit_index);
         if (bit_index > 0)
-            int_representation[i] += (id[byte_index + 1] >> @intCast(u3, 7 - bit_index + 1));
+            // int_representation[i] += (id[byte_index + 1] >> @intCast(u3, 7 - bit_index + 1));
+            int_representation[repr_index] += (id[byte_index + 1] >> @intCast(u3, 7 - bit_index + 1));
     }
 
     const factor = @intToFloat(f64, int_value) / std.math.pow(f64, 2, @sizeOf(u32) * 8 - 1);
-    std.log.info("nr: {}, n zero: {} int:{} pow:{} float:{}", .{ hex(&id), zero_bits, int_value, std.math.pow(u64, 2, @sizeOf(u32) * 8), factor });
 
     //todo, create the floating point such that 2**n * 1.xxxx can be calculated
     //we know n, need to get 1.xxxx so we can get log(2**n * 1.xxxx) = n + log(1.xxxx)
-    return @intToFloat(f64, zero_bits);
+    return @intToFloat(f64, 255 - zero_bits) + std.math.log(f64, 2, factor);
 }
