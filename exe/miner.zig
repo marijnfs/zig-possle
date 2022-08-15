40ead9e95d48c9139610e51ec3c52211b0ad9c55
Marijn Stollenga <m.stollenga@gmail.com>
2022-08-15 23:25:03 +0200

    updating block acceptance logic

--- exe/miner.zig
+++ exe/miner.zig
@@ -35,18 +35,38 @@ const Block = struct {
     total_difficulty: f64 = 0,
     prehash: ID = dht.id.zeroes(),
     embargo: i64 = 0,
+    total_embargo: i64 = 0,
 
-    pub fn calculate_prehash(block: *Block) Hash {
-        const prehash = pos.plot.hash_fast_mul(&.{ &block.prev, &block.tx, &block.nonce, std.mem.asBytes(&block.time), std.mem.asBytes(&block.height) });
+    pub fn calculate_prehash(block: *Block) void {
+        const prehash = pos.plot.hash_fast_mul(&.{
+            &block.prev,
+            &block.tx,
+            &block.nonce,
+            std.mem.asBytes(&block.time),
+            std.mem.asBytes(&block.height),
+        });
         block.prehash = prehash;
-        return block.prehash;
+    }
+
+    pub fn calculate_hash(block: *Block) void {
+        block.hash = pos.plot.hash_fast_mul(&.{
+            &block.prehash,
+            &block.seed,
+            &block.bud, //the proof
+            std.mem.asBytes(&block.difficulty),
+            std.mem.asBytes(&block.total_difficulty),
+            std.mem.asBytes(&block.embargo),
+        });
+    }
+
+    pub fn calculate_bud(block: *Block) void {
+        block.bud = try pos.plot.hash_slow(&block.seed);
     }
 
     pub fn calculate_difficulty(block: *Block) void {
-        block.bud = try pos.plot.hash_slow(&block.seed);
         const dist = dht.id.xor(block.prehash, block.bud);
         block.difficulty = distance_to_difficulty(dist);
-        block.embargo = 2.0 / block.difficulty;
+        block.embargo = @floatToInt(i64, 40.0 / block.difficulty * 1000.0);
     }
 };
 
@@ -57,7 +77,7 @@ const Api = union(enum) {
 var block_db: std.AutoHashMap(Hash, Block) = undefined;
 var chain_head = Block{};
 var our_block = Block{};
-// var closest_dist = dht.id.ones();
+var closest_dist = dht.id.ones();
 var highest_difficulty: f64 = 0;
 
 fn broadcast_hook(buf: []const u8, src_id: ID, src_address: net.Address) !void {
@@ -72,13 +92,13 @@ fn broadcast_hook(buf: []const u8, src_id: ID, src_address: net.Address) !void {
     std.log.info("tx:{}", .{hex(&block.tx)});
 
     std.log.info("new chain head block.height: {}", .{block.height});
-    chain_head = block; //todo, fix. This is dumb acceptance of the block
 
-    _ = block.calculate_prehash();
-    block.calculate_difficulty();
+    // Todo: Create copy and recalculate parameters to verify
+    // _ = block.calculate_prehash();
+    // block.calculate_difficulty();
 
     //origin block
-    var prev_height: f64 = chain_head.height;
+    // var prev_height: f64 = chain_head.height;
     // var prev_time: i64 = chain_head.time;
 
     // if (!std.mem.eql(u8, &block.prev, &std.mem.zeroes(Hash))) {
@@ -91,16 +111,17 @@ fn broadcast_hook(buf: []const u8, src_id: ID, src_address: net.Address) !void {
     // }
 
     // calculate the relative height of the block
-    const new_height = prev_height + 1;
-    const new_difficulty = chain_head.difficulty + block.difficulty;
-    std.log.info("height {} t:{}", .{ new_height, t });
+    // const new_height = prev_height + 1;
+    // std.log.info("height {} t:{}", .{ new_height, t });
+    // std.log.info("difficulty: {}", .{new_difficulty});
 
-    std.log.info("difficulty: {}", .{new_difficulty});
-
-    if (id_.less(new_difficulty, highest_difficulty)) {
+    if (block.total_difficulty > chain_head.total_difficulty) {
         // accept block
-        highest_difficulty = new_difficulty;
-        std.log.info("accepted {}", .{highest_difficulty});
+        highest_difficulty = block.total_difficulty;
+        chain_head = block;
+        closest_dist = dht.id.ones(); //reset closest
+
+        std.log.info("accepted {}", .{chain_head});
     }
 }
 
@@ -111,9 +132,14 @@ fn direct_message_hook(buf: []const u8, src_id: dht.ID, src_address: net.Address
 }
 
 fn setup_our_block(seed: dht.ID) void {
+    our_block.prev = chain_head.hash;
     our_block.seed = seed;
     our_block.time = time.milliTimestamp();
     our_block.height = chain_head.height + 1;
+
+    our_block.calculate_difficulty();
+    our_block.total_difficulty = chain_head.total_difficulty + our_block.total_difficulty;
+    our_block.total_embargo = chain_head.total_embargo + our_block.embargo;
 }
 
 pub fn main() anyerror!void {
@@ -121,9 +147,11 @@ pub fn main() anyerror!void {
 
     // Setup Chain block
     chain_head.time = time.milliTimestamp();
+    chain_head.total_embargo = time.milliTimestamp();
 
     // Setup Our block
     dht.rng.random().bytes(&our_block.tx); //our 'vote'
+    setup_our_block(id_.ones());
 
     // Setup server
     const options = try args.parseForCurrentProcess(struct {
@@ -168,48 +196,55 @@ pub fn main() anyerror!void {
     std.log.info("Start mining", .{});
     var i: usize = 0;
 
-    const Plant = pos.Plant;
-    var closest = std.mem.zeroes(Plant);
-
     try server.start();
     try server.queue_broadcast("hello");
 
     while (true) {
-        // Setup our_block
-        // Update nonce and perhaps tx
-        dht.rng.random().bytes(&our_block.nonce);
-
-        // Get prehash
-        const prehash = our_block.calculate_prehash();
-        const search_plant = Plant{ .bud = prehash };
-
-        const found = try persistent_merged_loaded.find(prehash);
-
-        // const found = try indexed_plot.find(bud);
-        const dist = dht.id.xor(found.bud, search_plant.bud);
-
-        if (std.mem.order(u8, &dist, &closest_dist) == .lt) {
-            our_block.seed = found.seed;
-            closest = found;
-            closest_dist = dist;
-            setup_our_block(found.seed);
-
-            std.log.info("\r[{}] persistent search:dist:{} {} got:{}", .{
-                i,
-                hex(&closest_dist),
-                hex(&search_plant.bud),
-                hex(&found.bud),
-            });
-
-            const difficulty = distance_to_difficulty(closest_dist);
-            const embargo = 2.0 / difficulty;
-
-            std.log.info("log:{} difficulty:{} embargo:{}", .{ log_value, difficulty, embargo });
+        const t = time.milliTimestamp();
+        if (t > our_block.total_embargo) { //time to send block
+            std.log.info("embargo passed, sending time: {}, embargo: {}", .{ t, our_block.total_embargo });
 
             const msg = Api{ .block = our_block };
             const buf = try dht.serial.serialise_alloc(msg, allocator);
             // defer allocator.free(msg);
             try server.queue_broadcast(buf);
+
+            //accept our new block
+            chain_head = our_block;
+            closest_dist = dht.id.ones(); //reset closest
+            setup_our_block(id_.ones());
+        }
+
+        // Setup our_block
+        // Update nonce and perhaps tx
+        dht.rng.random().bytes(&our_block.nonce);
+        our_block.time = t;
+
+        // Get prehash
+        our_block.calculate_prehash();
+        const found = try persistent_merged_loaded.find(our_block.prehash);
+
+        // const found = try indexed_plot.find(bud);
+        const dist = dht.id.xor(found.bud, our_block.prehash);
+
+        if (std.mem.order(u8, &dist, &closest_dist) == .lt) {
+            our_block.seed = found.seed;
+            our_block.bud = found.bud;
+
+            closest_dist = dist;
+            setup_our_block(found.seed);
+
+            // std.log.info("\r[{}] persistent search:dist:{} {} got:{}", .{
+            //     i,
+            //     hex(&closest_dist),
+            //     hex(&our_block.prehash),
+            //     hex(&found.bud),
+            // });
+
+            // const difficulty = distance_to_difficulty(closest_dist);
+            // const embargo = 40.0 / difficulty;
+
+            // std.log.info("difficulty:{} embargo:{}", .{ difficulty, embargo });
         }
 
         i += 1;
