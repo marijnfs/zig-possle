afc4fdd1ecaab6661760a894690901ff23b01993
Marijn Stollenga <m.stollenga@gmail.com>
2022-08-23 03:01:14 +0200

    script stuff

--- client_a.sh
+++ client_a.sh
@@ -1,2 +1,3 @@
 #!/bin/env sh
-./zig-out/bin/miner --ip 0.0.0.0 --port 8091 --remote_ip 0.0.0.0 --remote_port 8090 --plot_path main.db
+./zig-out/bin/miner --ip 0.0.0.0 --port 9091 --remote_ip 0.0.0.0 --remote_port 8090 --plot_path main.db --req_thread 1
+
--- server.sh
+++ server.sh
@@ -1,2 +1,2 @@
 #!/bin/env sh
-./zig-out/bin/miner --ip 0.0.0.0 --port 8090 --plot_path main.db
+./zig-out/bin/miner --ip 0.0.0.0 --port 8090 --plot_path main.db --req_thread 1
